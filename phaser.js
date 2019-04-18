2b05253e7c3b7e5ddde2603f606dc79f4d53693b
MissionBit <missionbit@81p-MissionBit.local>
2019-04-17 17:55:06 -0700

    made the floor and the character
    the character is there but its not correct? like too many frames

--- phaser.js
+++ phaser.js
@@ -21,34 +21,66 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('background', 'background.png');
-    this.load.image('sky','assets/sky.png');
-    this.load.image('ground','assets/platform.png');
-    this.load.spritesheet('dude','assets/dude.png', 
-    { frameWidth:32, frameHeight: 48}
-    );
+
+    this.load.image('dude', 'kagato_sprites.gif')
+    
+this.anims.create({
+    key: 'left',
+    frames:
+this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+
+})
 }
 var platform;
 
 function create ()
 {
-    
+  
     this.cameras.main.setBackgroundColor('#ffe680');
     
 
     platforms = this.physics.add.staticGroup();
 
-    platforms.create(60, 568,
-'ground').setScale(2).refreshBody();
-    //platforms.create(400, 568,'ground').setScale(2).refreshBody();
-
-   //  platform.create(600,400, 'ground');
-    // platform.create(50, 250, 'ground');
-    //platform.create(750, 220, 'ground');
+    platforms.create(90, 468,
+'ground').setScale(1.5).refreshBody();
+   
+     platforms.create(280, 470, 'ground');
+     platforms.create(400, 400, 'ground');
+     platforms.create(175, 520, 'ground');
 
     this.add.image(300,580,'ground')
+
+    player = this.physics.add.sprite(100, 403, 'dude',);
+
+player.setBounce(0.2);
+player.setCollideWorldBounds(true);
+
+this.anims.create({
+    key: 'left',
+    frames:
+this.anims.generateFrameNumbers('dude', { start:0, end: 1}),
+    frameRate:10,
+    repeat: -1
+});
+
+this.anims.create({
+    key: 'turn',
+    frames: [{key: 'dude', frame:4} ],
+    frameRate: 20
+});
+
+this.anims.create({
+    key: 'right',
+    frames:
+this.anims.generateFrameNumbers('dude', {start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+})
 }
 
 function update ()
 {
-}
 
+}
